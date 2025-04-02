508e14503def71d37f3c187644c2f2f3176be2b1
Bart Rayson <brayson@live.com>
2025-04-02 09:42:15 +0100

    added basic styling to homepage

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,12 +1,12 @@
-import { Link } from 'react-router-dom'
-
+import { Link } from 'react-router-dom';
+import "./Home.css"
 const Home = () => {
-    return (
-        <div> 
-            <h1>Sky Skills Assessment</h1>
-            <Link to="/assessment">Assessment</Link> 
-        </div>
-    )
-}
+  return (
+    <div className="home-container">
+      <h1>Sky Skills Assessment</h1>
+      <Link to="/assessment">Start Assessment</Link> 
+    </div>
+  );
+};
 
 export default Home;
