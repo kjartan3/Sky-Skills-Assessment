02fe006a4fc3f4ad0b69d12cbce5a6d9a34bc006
Kjartan Heimisson <kheimisson@gmail.com>
2025-06-05 11:32:23 +0100

    styling changes for homepage

--- frontend/src/components/TitleVideo.js
+++ frontend/src/components/TitleVideo.js
@@ -1,11 +1,12 @@
 import React from "react";
+import { Link } from 'react-router-dom';
 import './TitleVideo.css';
 
 const TitleVideo = () => {
     return (
         <div className="title-video-wrapper">
             <div className="title-video-container">
-                <h1 className="title-video-heading">Skills</h1>
+                {/* <h1 className="title-video-heading">Sky Skills Assessment</h1> */}
                 <div className="title-video-iframe-wrapper">
                 <iframe
                     src="https://share.synthesia.io/embeds/videos/9c7aa145-ae11-4fa5-be8c-178e0ec7cb71?language=en"
@@ -15,7 +16,25 @@ const TitleVideo = () => {
                     allow="encrypted-media; fullscreen;"
                     className="title-video-iframe"
                 ></iframe>
+
                 </div>
+                <div className="link-boxes">
+
+
+                <div className="box">
+                    <h2>Assessment</h2>
+                    {/* <img src="/icons/3d-cube.png" alt="Assessment" /> */}
+                    <Link to="/assessment" className="btn">Start</Link>
+                </div>
+
+                <div className="box">
+                    <h2>Summary</h2>
+                    {/* <img src="/icons/analytics.png" alt="Summary" /> */}
+                    <Link to="/assessmentsummary" className="btn">View</Link>
+                </div>
+
+               
+            </div>
             </div>
         </div>
     );
--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,7 +1,7 @@
 import React, { useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import './Home.css';
-
+import TitleVideo from '../components/TitleVideo.js';
 
 
     const Home = ({ user }) => {
@@ -16,35 +16,8 @@ import './Home.css';
 
     return (
         <div className="home-container">
-            <h1>Sky Skills Assessment</h1>
-           
-            
-
-            <div className="link-boxes">
-            <div className="box">
-                    <h2>Introduction Video</h2>
-                    <img src="/icons/online-learning.png" alt="Skills" />
-                    <Link to="/skills" className="btn">Explore</Link>
-                </div>
-                <div className="box">
-                    <h2>Assessment</h2>
-                    <img src="/icons/3d-cube.png" alt="Assessment" />
-                    <Link to="/assessment" className="btn">Start</Link>
-                </div>
-
-                <div className="box">
-                    <h2>Summary</h2>
-                    <img src="/icons/analytics.png" alt="Summary" />
-                    <Link to="/assessmentsummary" className="btn">View</Link>
-                </div>
-
-                {/* <div className="box">
-                    <h2>Profile</h2>
-                    <img src="/icons/user-2.png" alt="Profile" />
-                    <Link to="/profile" className="btn">Open</Link>
-                </div> */}
-
-               
+            <div>
+                <TitleVideo />
             </div>
         </div>
     );
